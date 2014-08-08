697ef49a380d735690725219519ad757ba7a36b3
Gordon Watts <gordon@gordonandpaula.net>
2014-08-07 23:28:13 -0700

    Updates to work with new root package
    Fixed up the problem with the shared library guy

--- GetSharedLibraryList/main.cpp
+++ GetSharedLibraryList/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
 #include "TClass.h"
+#include "TApplication.h"
+#include "TInterpreter.h"
 
 using namespace std;
 
 int main()
 {
+	// You can't do this without initializing TCint. The easiest way to do this is
+	// by creating the TApplication object.
+
+	int args = 0;
+	char **argv = 0;
+	auto t = new TApplication("hi there", &args, argv);
+
+	// Get the class pointer and then the list of objects.
+	
 	auto a = TClass::GetClass("TApplication");
 	if (a == nullptr) {
 		cout << "Unable to get the TApplication class pointer!" << endl;
