a6e394a54be4d8c439e9fd60f0df7e1c6ea5b3af
Gordon Watts <gordon@gordonandpaula.net>
2015-08-09 12:57:40 +0200

    Update to recent version of package

---
+++ TestLinkage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLinkage
+{
+    /// <summary>
+    /// This project is not meant to access .NET directly - it is just to see if when we include the ROOT package
+    /// it causes "bad" things to happen.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
